c238ca02dfc6be2a0d520ec0a1577692b4af9828
Joe Landers <jcl696@stern.nyu.edu>
2019-06-16 22:23:09 -0400

    latest closing price
    defining a function to pass $ and correct format as well

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -10,19 +10,21 @@ import json
 requests_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(requests_url)
-#print(type(response))  # > <class 'requests.models.Response'>
-#print(response.status_code) #> 200 
-#print(response.text)
+ 
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+# > $1,000
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
+
 
 #breakpoint()
 
 
-
+def to_usd(my_price): #reference an int or flaot with to_usd to get the dollar sign and correct decimals
+    return "${0:,.2f}".format(my_price)
 
 
 
@@ -34,10 +36,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #TODO - use the datetime module to do this
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
