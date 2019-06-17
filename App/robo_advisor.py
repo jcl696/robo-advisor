14ddf24b9c9bec66627bc8660043ac3a8c1b8eaa
Joe Landers <jcl696@stern.nyu.edu>
2019-06-16 22:54:07 -0400

    return high price
    latest high price based on last 100 days

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -34,6 +34,24 @@ latest_close = tsd[latest_day]["4. close"]
 #breakpoint()
 
 
+#maximum of the last 100 high prices
+#high_price = [10,20,30,40,1233]
+#recent_high = max(high_price)
+
+
+high_prices = []
+
+
+for date in dates: 
+    high_price = tsd[date]["2. high"] 
+    high_prices.append(float(high_price))
+
+recent_high = max(high_prices)
+
+#breakpoint()
+
+
+
 def to_usd(my_price): #reference an int or flaot with to_usd to get the dollar sign and correct decimals
     return "${0:,.2f}".format(my_price)
 
@@ -51,7 +69,7 @@ print("REQUEST AT: 2018-02-20 02:00pm") #TODO - use the datetime module to do th
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
