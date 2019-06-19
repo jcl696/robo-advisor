0b2fa9733a8469599440feaae7dd7ad60d2ac317
Joe Landers <jcl696@stern.nyu.edu>
2019-06-19 18:46:27 -0400

    while loop for validation requirements

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -7,7 +7,7 @@
 #KEEP MODULES BEFORE PACKAGES
 
 #TODO - BRING IN THE CSV MODULE AND START WRITING 
-
+import datetime
 import csv
 import json
 import os
@@ -17,25 +17,46 @@ import requests
 
 load_dotenv()
 
+#TODO - Make stock choice an input with a fail message if the stock is wrong
+# use the datetime module to do this  ----> DONE
+# recommendaiton and reason 
 
-stock_choice = "MSFT" #input("What stock would you like to have analyzed today? ")
+#stock_choice = input("What stock would you like to have analyzed today? ")
+
+
+# Checking for errors in the input 
 API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-requests_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_choice}&apikey={API_KEY}"
 
-response = requests.get(requests_url)
- 
+while True:
+    stock_choice = input("What stock would you like to have analyzed today? ")
+    requests_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_choice}&apikey={API_KEY}"
+    response = requests.get(requests_url)
+    if "Error" in response.text:
+        print("The stock you submitted doesn't exist.")
+        #print(response.text)
+    
+    else: 
+        break 
+
+
+
+
+
+
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-#breakpoint()
+
+
 
 tsd = parsed_response["Time Series (Daily)"]
 
 dates = list(tsd.keys()) # TODO - SORT: assumes latest day is first to make sure the latest day is first
 
+#dates.sort()
 
 latest_day = dates[0]  # > 2019-06-17
 
@@ -99,20 +120,22 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
     #writer.writerow({"city": "New Haven", "name": "Ravens"})
 #
 
+now = datetime.datetime.now()
+right_now = (now.strftime("%Y-%m-%d %H:%M:%S %p"))
 
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm") #TODO - use the datetime module to do this
+print(f"REQUEST AT: {right_now}")  
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION: BUY!") 
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print(f"WRITE DATA TO CSV: {csv_file_path}")
